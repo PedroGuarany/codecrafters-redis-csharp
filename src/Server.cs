8b6f592b301dbf940fc4fa84afeae9b9a78f0827
Guarany <jp.tex@hotmail.com>
2024-03-20 20:57:58 -0300

    feat: stage 2 accept ping

--- src/Server.cs
+++ src/Server.cs
@@ -1,8 +1,47 @@
 using System.Net;
 using System.Net.Sockets;
 
+const string PONG = "+PONG\r\n";
 Console.WriteLine("Program started!");
 
-var TCPListener = new TcpListener(IPAddress.Any, 6379);
-TCPListener.Start();
-TCPListener.AcceptSocket();
+var server = new TcpListener(IPAddress.Any, 6379);
+server.Start();
+//server.AcceptSocket();
+
+try
+{
+    var bytes = new byte[256];
+    while (true)
+    {
+        Console.WriteLine("Waiting for connection...");
+        // Perform a blocking call to accept requests.
+        // You could also use server.AcceptSocket() here.
+        using TcpClient client = server.AcceptTcpClient();
+        Console.WriteLine("Connected!");
+
+        var stream = client.GetStream();
+
+
+        // Loop to receive all the data sent by the client.
+        var byteSize = 0;
+        while ((byteSize = stream.Read(bytes, 0, bytes.Length)) != 0)
+        {
+            string request = System.Text.Encoding.ASCII.GetString(bytes, 0, byteSize);
+            request = request.ToUpper();
+            Console.WriteLine("Received: {0}", request);
+
+            var response = System.Text.Encoding.ASCII.GetBytes(PONG);
+
+            stream.Write(response, 0, response.Length);
+            Console.WriteLine("Sent: {0}", response);
+        }
+    }
+}
+catch (SocketException e)
+{
+    Console.WriteLine("SocketException: {0}", e);
+}
+finally
+{
+    server.Stop();
+}
