361d0cd4ec9a399d1d96d3de91cef50cf17c91d0
Guarany <jp.tex@hotmail.com>
2024-03-20 22:39:25 -0300

    feat: stage 4 accept multiple connections

--- src/Server.cs
+++ src/Server.cs
@@ -2,29 +2,23 @@ using System.Net;
 using System.Net.Sockets;
 
 const string PONG = "+PONG\r\n";
+var callbackQueue = new Queue<Action>();
 Console.WriteLine("Program started!");
 
 var server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
 
+callbackQueue.Enqueue(() => { Console.WriteLine("Testando"); });
 try
 {
-    var bytes = new byte[256];
-    using var socket = server.AcceptSocket();
-    while (socket.Connected)
+    while (true)
     {
-        var byteSize = await socket.ReceiveAsync(bytes, SocketFlags.None);
-
-        if (byteSize == 0) break;
-
-        string request = System.Text.Encoding.ASCII.GetString(bytes, 0, byteSize);
-        request = request.ToUpper();
-        Console.WriteLine("Received: {0}", request);
-
-        var response = System.Text.Encoding.ASCII.GetBytes(PONG);
-
-        await socket.SendAsync(response, SocketFlags.None);
-        Console.WriteLine("Sent: {0}", response);
+        var socket = server.AcceptSocket();
+        if (socket.Connected)
+        {
+            var newThread = new Thread(() => HandleSocketConnection(socket));
+            newThread.Start();
+        }
     }
 }
 catch (SocketException e)
@@ -34,4 +28,34 @@ catch (SocketException e)
 finally
 {
     server.Stop();
+}
+
+static void HandleSocketConnection(Socket socket)
+{
+    try
+    {
+        Console.WriteLine("Connected");
+        while (socket.Connected)
+        {
+            var bytes = new byte[256];
+            var byteSize = socket.Receive(bytes, SocketFlags.None);
+
+            if (byteSize == 0) break;
+
+            string request = System.Text.Encoding.ASCII.GetString(bytes, 0, byteSize);
+            request = request.ToUpper();
+
+            var response = System.Text.Encoding.ASCII.GetBytes(PONG);
+
+            socket.Send(response, SocketFlags.None);
+        }
+    }
+    catch (SocketException e)
+    {
+        Console.WriteLine("SocketException: {0}", e.Message);
+    }
+    finally
+    {
+        socket.Dispose();
+    }
 }
